b4478df278303c5a56a0ba5cbefcec7406733c65
Jonathan-Greve <jonagrev@gmail.com>
2022-12-27 20:54:29 +0800

    Added GWIPC namespace

--- GWIPC/SharedMemory.h
+++ GWIPC/SharedMemory.h
@@ -3,65 +3,66 @@
 #include <string>
 #include <format>
 
-class SharedMemory {
-public:
-	// Constructs a new shared memory object with the given name and size.
-	// If the object already exists, it will be opened instead of created.
-	// Constructs a new shared memory object with the given name and size.
-	// If the object already exists, it will be opened instead of created.
-	SharedMemory(std::string name, uint32_t size) : sm_name_(name), sm_size_(size)
-	{
-		mutex_ = CreateMutexA(NULL, FALSE, (name + "mutex").c_str());
-		if (mutex_ == NULL) {
-			throw std::format("Could not create mutex ({})\n", GetLastError());
+namespace GWIPC {
+	class SharedMemory {
+	public:
+		// Constructs a new shared memory object with the given name and size.
+		// If the object already exists, it will be opened instead of created.
+		// Constructs a new shared memory object with the given name and size.
+		// If the object already exists, it will be opened instead of created.
+		SharedMemory(std::string name, uint32_t size) : sm_name_(name), sm_size_(size)
+		{
+			mutex_ = CreateMutexA(NULL, FALSE, (name + "mutex").c_str());
+			if (mutex_ == NULL) {
+				throw std::format("Could not create mutex ({})\n", GetLastError());
+			}
+
+			mapping_ = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, size, name.c_str());
+			if (mapping_ == NULL) {
+				throw std::format("Could not create file mapping object ({})\n", GetLastError());
+			}
+
+			data_ = MapViewOfFile(mapping_, FILE_MAP_ALL_ACCESS, 0, 0, size);
+			if (data_ == NULL) {
+				throw std::format("Could not map view of file ({})\n", GetLastError());
+			}
 		}
 
-		mapping_ = CreateFileMappingA(INVALID_HANDLE_VALUE, NULL, PAGE_READWRITE, 0, size, name.c_str());
-		if (mapping_ == NULL) {
-			throw std::format("Could not create file mapping object ({})\n", GetLastError());
+		// Destroys the shared memory object.
+		// Destroys the shared memory object.
+		~SharedMemory() {
+			UnmapViewOfFile(data_);
+			CloseHandle(mapping_);
+			CloseHandle(mutex_);
 		}
 
-		data_ = MapViewOfFile(mapping_, FILE_MAP_ALL_ACCESS, 0, 0, size);
-		if (data_ == NULL) {
-			throw std::format("Could not map view of file ({})\n", GetLastError());
+		// Returns a pointer to the shared memory.
+		// Returns a pointer to the shared memory.
+		void* data() const {
+			return data_;
 		}
-	}
 
-	// Destroys the shared memory object.
-	// Destroys the shared memory object.
-	~SharedMemory() {
-		UnmapViewOfFile(data_);
-		CloseHandle(mapping_);
-		CloseHandle(mutex_);
-	}
+		// Locks the shared memory for reading or writing.
+		// Locks the shared memory for reading or writing.
+		void lock() {
+			WaitForSingleObject(mutex_, INFINITE);
+		}
 
-	// Returns a pointer to the shared memory.
-	// Returns a pointer to the shared memory.
-	void* data() const {
-		return data_;
-	}
+		// Unlocks the shared memory.
+		// Unlocks the shared memory.
+		void unlock() {
+			ReleaseMutex(mutex_);
+		}
 
-	// Locks the shared memory for reading or writing.
-	// Locks the shared memory for reading or writing.
-	void lock() {
-		WaitForSingleObject(mutex_, INFINITE);
-	}
+		const std::string& get_sm_name() const { return sm_name_; }
+		const uint32_t  get_sm_size() const { return sm_size_; }
 
-	// Unlocks the shared memory.
-	// Unlocks the shared memory.
-	void unlock() {
-		ReleaseMutex(mutex_);
-	}
+	private:
+		std::string sm_name_;
+		uint32_t sm_size_;
 
-	const std::string& get_sm_name() const { return sm_name_; }
-	const uint32_t  get_sm_size() const { return sm_size_; }
-
-private:
-	std::string sm_name_;
-    uint32_t sm_size_;
-
-	HANDLE mutex_;
-	HANDLE mapping_;
-	void* data_;
-};
-
+		HANDLE mutex_;
+		HANDLE mapping_;
+		void* data_;
+	};
+}
--- GWIPC/SharedMemoryLock.h
+++ GWIPC/SharedMemoryLock.h
@@ -1,18 +1,21 @@
 #pragma once
 #include "SharedMemory.h"
 
-class SharedMemoryLock {
-public:
-    // Locks the shared memory object.
-    SharedMemoryLock(SharedMemory& shm) : shm_(shm) {
-        shm_.lock();
-    }
 
-    // Unlocks the shared memory object.
-    ~SharedMemoryLock() {
-        shm_.unlock();
-    }
+namespace GWIPC {
+    class SharedMemoryLock {
+    public:
+        // Locks the shared memory object.
+        SharedMemoryLock(SharedMemory& shm) : shm_(shm) {
+            shm_.lock();
+        }
 
-private:
-    SharedMemory& shm_;
-};
+        // Unlocks the shared memory object.
+        ~SharedMemoryLock() {
+            shm_.unlock();
+        }
+
+    private:
+        SharedMemory& shm_;
+    };
+}
