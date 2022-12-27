4ec7de76a5d28553f471d6a2328afc6d1ae2cf78
Jonathan-Greve <jonagrev@gmail.com>
2022-12-27 20:39:00 +0800

    Implemented the connection manager class

--- GWIPC/ConnectionManager.h
+++ GWIPC/ConnectionManager.h
@@ -2,36 +2,92 @@
 #include "SharedMemoryLock.h"
 #include "connections_generated.h"
 #include <vector>
+#include <algorithm>
 
 class ConnectionManager {
 public:
-	ConnectionManager(const std::string& connection_id) : shared_memory_(SharedMemory(sm_name, sm_size)),
+	ConnectionManager(const std::string& connection_id) : shared_memory_(SharedMemory(sm_name_, sm_size_)),
 		connection_id_(connection_id)
 	{
+		// Create or open the event. This will be used to notify other processes and a client connect or disconnect.
+		event_ = ::CreateEventA(nullptr, TRUE, FALSE, event_name_.c_str());
+		if (event_ == nullptr) {
+			throw "Could not create connections event";
+		}
 	}
 
 	void connect() {
 		SharedMemoryLock lock(shared_memory_); // Scoped lock
-	};
+
+		auto curr_connection_ids = get_connections_ids();
+		const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
+		if (it == curr_connection_ids.end()) {
+			curr_connection_ids.push_back(connection_id_);
+
+			write_connections_to_sm(curr_connection_ids);
+		}
+
+		SetEvent(event_);
+	}
 
 	void disconnect(){
 		SharedMemoryLock lock(shared_memory_);
+
+		auto curr_connection_ids = get_connections_ids();
+		const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
+		if (it != curr_connection_ids.end()) {
+			curr_connection_ids.erase(it);
+
+			write_connections_to_sm(curr_connection_ids);
+		}
+
+		SetEvent(event_);
 	}
+
 	// Returns a list of all connected email addresses.
-	std::vector<std::string> get_connections() {
+	std::vector<std::string> get_connections_ids() {
+		std::vector<std::string> result;
+
+		SharedMemoryLock lock(shared_memory_);
 		uint8_t* sm_data = static_cast<uint8_t*>(shared_memory_.data());
 		if (sm_data) {
 			auto connections = GWIPC::GetConnections(sm_data);
 			if (connections) {
-				// Check 
+				// Check
+				auto ids = connections->connected_ids();
+				if (ids){
+					for (const auto id : *ids) {
+						result.push_back(id->str());
+					}
+				}
 			}
 		}
-	};
+
+		return result;
+	}
 
 private:
 	std::string connection_id_;
 
 	SharedMemory shared_memory_;
-	const std::string sm_name = "connections";
-	const uint32_t sm_size = 1024;
+	const std::string sm_name_ = "connections";
+	const uint32_t sm_size_ = 1024;
+
+	HANDLE event_;
+	const std::string event_name_ = "connections_event";
+
+
+	void write_connections_to_sm(const std::vector<std::string>& connections) {
+		// Build flatbuffer
+		flatbuffers::FlatBufferBuilder builder(shared_memory_.get_sm_size());
+		auto new_connection_ids = builder.CreateVectorOfStrings(connections);
+		auto connections_flatbuf = GWIPC::CreateConnections(builder, new_connection_ids);
+		builder.Finish(connections_flatbuf);
+
+		// Copy and overwrite shared memory.
+		uint8_t* buf = builder.GetBufferPointer();
+		int size = builder.GetSize();
+		uint8_t* data = static_cast<uint8_t*>(shared_memory_.data());
+		memcpy(data, buf, size);
+	}
 };
--- GWIPC/SharedMemory.h
+++ GWIPC/SharedMemory.h
@@ -9,7 +9,8 @@ public:
 	// If the object already exists, it will be opened instead of created.
 	// Constructs a new shared memory object with the given name and size.
 	// If the object already exists, it will be opened instead of created.
-	SharedMemory(std::string name, uint32_t size) {
+	SharedMemory(std::string name, uint32_t size) : sm_name_(name), sm_size_(size)
+	{
 		mutex_ = CreateMutexA(NULL, FALSE, (name + "mutex").c_str());
 		if (mutex_ == NULL) {
 			throw std::format("Could not create mutex ({})\n", GetLastError());
@@ -52,7 +53,13 @@ public:
 		ReleaseMutex(mutex_);
 	}
 
+	const std::string& get_sm_name() const { return sm_name_; }
+	const uint32_t  get_sm_size() const { return sm_size_; }
+
 private:
+	std::string sm_name_;
+    uint32_t sm_size_;
+
 	HANDLE mutex_;
 	HANDLE mapping_;
 	void* data_;
