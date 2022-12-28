830efb6a8ff5525c04cf4773acedc3133241b0dc
Jonathan-Greve <jonagrev@gmail.com>
2022-12-28 21:06:23 +0800

    Minor additions to API

--- GWIPC/ConnectionManager.h
+++ GWIPC/ConnectionManager.h
@@ -4,90 +4,116 @@
 #include <vector>
 #include <algorithm>
 
-namespace GWIPC {
-	class ConnectionManager {
-	public:
-		ConnectionManager(const std::string& connection_id) : shared_memory_("connections", 1024),
-			connection_id_(connection_id)
-		{
-			// Create or open the event. This will be used to notify other processes and a client connect or disconnect.
-			event_ = ::CreateEventA(nullptr, TRUE, FALSE, event_name_.c_str());
-			if (event_ == nullptr) {
-				throw "Could not create connections event";
-			}
-		}
+namespace GWIPC
+{
+class ConnectionManager
+{
+public:
+    ConnectionManager()
+        : shared_memory_("connections", 1024)
+    {
+        create_event();
+    }
 
-		void connect() {
-			SharedMemoryLock lock(shared_memory_); // Scoped lock
+    ConnectionManager(const std::string& connection_id)
+        : shared_memory_("connections", 1024)
+        , connection_id_(connection_id)
+    {
+        create_event();
+    }
 
-			auto curr_connection_ids = get_connections_ids();
-			const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
-			if (it == curr_connection_ids.end()) {
-				curr_connection_ids.push_back(connection_id_);
+    void connect()
+    {
+        SharedMemoryLock lock(shared_memory_); // Scoped lock
 
-				write_connections_to_sm(curr_connection_ids);
-			}
+        auto curr_connection_ids = get_connections_ids();
+        const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
+        if (it == curr_connection_ids.end())
+        {
+            curr_connection_ids.push_back(connection_id_);
 
-			SetEvent(event_);
-		}
+            write_connections_to_sm(curr_connection_ids);
+        }
 
-		void disconnect() {
-			SharedMemoryLock lock(shared_memory_);
+        SetEvent(event_);
+    }
 
-			auto curr_connection_ids = get_connections_ids();
-			const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
-			if (it != curr_connection_ids.end()) {
-				curr_connection_ids.erase(it);
+    void disconnect()
+    {
+        SharedMemoryLock lock(shared_memory_);
 
-				write_connections_to_sm(curr_connection_ids);
-			}
+        auto curr_connection_ids = get_connections_ids();
+        const auto it = std::find(curr_connection_ids.begin(), curr_connection_ids.end(), connection_id_);
+        if (it != curr_connection_ids.end())
+        {
+            curr_connection_ids.erase(it);
 
-			SetEvent(event_);
-		}
+            write_connections_to_sm(curr_connection_ids);
+        }
 
-		// Returns a list of all connected email addresses.
-		std::vector<std::string> get_connections_ids() {
-			std::vector<std::string> result;
+        SetEvent(event_);
+    }
 
-			SharedMemoryLock lock(shared_memory_);
-			uint8_t* sm_data = static_cast<uint8_t*>(shared_memory_.data());
-			if (sm_data) {
-				auto connections = GWIPC::GetConnections(sm_data);
-				if (connections) {
-					// Check
-					auto ids = connections->connected_ids();
-					if (ids) {
-						for (const auto id : *ids) {
-							result.push_back(id->str());
-						}
-					}
-				}
-			}
+    // Returns a list of all connected email addresses.
+    std::vector<std::string> get_connections_ids()
+    {
+        std::vector<std::string> result;
 
-			return result;
-		}
+        uint8_t* sm_data = static_cast<uint8_t*>(shared_memory_.data());
+        if (sm_data)
+        {
+            auto connections = GWIPC::GetConnections(sm_data);
+            if (connections)
+            {
+                // Check
+                auto ids = connections->connected_ids();
+                if (ids)
+                {
+                    for (const auto id : *ids)
+                    {
+                        result.push_back(id->str());
+                    }
+                }
+            }
+        }
 
-	private:
-		std::string connection_id_ = "not set";
+        return result;
+    }
 
-		SharedMemory shared_memory_;
+    HANDLE get_event_handle() { return event_; }
+    HANDLE get_mutex_handle() { return shared_memory_.get_mutex_handle(); }
 
-		HANDLE event_;
-		const std::string event_name_ = "connections_event";
+private:
+    std::string connection_id_ = "not set";
 
+    SharedMemory shared_memory_;
 
-		void write_connections_to_sm(const std::vector<std::string>& connections) {
-			// Build flatbuffer
-			flatbuffers::FlatBufferBuilder builder(shared_memory_.get_sm_size());
-			auto new_connection_ids = builder.CreateVectorOfStrings(connections);
-			auto connections_flatbuf = GWIPC::CreateConnections(builder, new_connection_ids);
-			builder.Finish(connections_flatbuf);
+    HANDLE event_;
+    const std::string event_name_ = "connections_event";
 
-			// Copy and overwrite shared memory.
-			uint8_t* buf = builder.GetBufferPointer();
-			int size = builder.GetSize();
-			uint8_t* data = static_cast<uint8_t*>(shared_memory_.data());
-			memcpy(data, buf, size);
-		}
-	};
+    void write_connections_to_sm(const std::vector<std::string>& connections)
+    {
+        // Build flatbuffer
+        flatbuffers::FlatBufferBuilder builder(shared_memory_.get_sm_size());
+        auto new_connection_ids = builder.CreateVectorOfStrings(connections);
+        auto connections_flatbuf = GWIPC::CreateConnections(builder, new_connection_ids);
+        builder.Finish(connections_flatbuf);
+
+        // Copy and overwrite shared memory.
+        uint8_t* buf = builder.GetBufferPointer();
+        int size = builder.GetSize();
+        uint8_t* data = static_cast<uint8_t*>(shared_memory_.data());
+        memcpy(data, buf, size);
+    }
+
+    void create_event()
+    {
+        // Create or open the event. This will be used to notify other processes and a client connect or disconnect.
+        event_ = ::CreateEventA(nullptr, TRUE, FALSE, event_name_.c_str());
+        if (event_ == nullptr)
+        {
+            throw "Could not create connections event";
+        }
+    }
+};
 }
