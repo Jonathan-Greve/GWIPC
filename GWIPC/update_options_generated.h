2a5eedc8b9119ca9c776db9a1a0f134562053b74
Jonathan-Greve <jonagrev@gmail.com>
2023-06-19 13:12:56 +0800

    Added option to disable client updates.

--- GWIPC/update_options_generated.h
+++ GWIPC/update_options_generated.h
@@ -22,7 +22,8 @@ struct UpdateOptions FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
   typedef UpdateOptionsBuilder Builder;
   enum FlatBuffersVTableOffset FLATBUFFERS_VTABLE_UNDERLYING_TYPE {
     VT_ONLY_SEND_ACTIVE_QUEST_DESCRIPTION = 4,
-    VT_ONLY_SEND_ACTIVE_QUEST_OBJECTIVES = 6
+    VT_ONLY_SEND_ACTIVE_QUEST_OBJECTIVES = 6,
+    VT_SHOULD_UPDATE_CLIENT_DATA = 8
   };
   bool only_send_active_quest_description() const {
     return GetField<uint8_t>(VT_ONLY_SEND_ACTIVE_QUEST_DESCRIPTION, 0) != 0;
@@ -30,10 +31,14 @@ struct UpdateOptions FLATBUFFERS_FINAL_CLASS : private flatbuffers::Table {
   bool only_send_active_quest_objectives() const {
     return GetField<uint8_t>(VT_ONLY_SEND_ACTIVE_QUEST_OBJECTIVES, 0) != 0;
   }
+  bool should_update_client_data() const {
+    return GetField<uint8_t>(VT_SHOULD_UPDATE_CLIENT_DATA, 0) != 0;
+  }
   bool Verify(flatbuffers::Verifier &verifier) const {
     return VerifyTableStart(verifier) &&
            VerifyField<uint8_t>(verifier, VT_ONLY_SEND_ACTIVE_QUEST_DESCRIPTION, 1) &&
            VerifyField<uint8_t>(verifier, VT_ONLY_SEND_ACTIVE_QUEST_OBJECTIVES, 1) &&
+           VerifyField<uint8_t>(verifier, VT_SHOULD_UPDATE_CLIENT_DATA, 1) &&
            verifier.EndTable();
   }
 };
@@ -47,6 +52,9 @@ struct UpdateOptionsBuilder {
   }
   void add_only_send_active_quest_objectives(bool only_send_active_quest_objectives) {
     fbb_.AddElement<uint8_t>(UpdateOptions::VT_ONLY_SEND_ACTIVE_QUEST_OBJECTIVES, static_cast<uint8_t>(only_send_active_quest_objectives), 0);
+  }
+  void add_should_update_client_data(bool should_update_client_data) {
+    fbb_.AddElement<uint8_t>(UpdateOptions::VT_SHOULD_UPDATE_CLIENT_DATA, static_cast<uint8_t>(should_update_client_data), 0);
   }
   explicit UpdateOptionsBuilder(flatbuffers::FlatBufferBuilder &_fbb)
         : fbb_(_fbb) {
@@ -62,8 +70,10 @@ struct UpdateOptionsBuilder {
 inline flatbuffers::Offset<UpdateOptions> CreateUpdateOptions(
     flatbuffers::FlatBufferBuilder &_fbb,
     bool only_send_active_quest_description = false,
-    bool only_send_active_quest_objectives = false) {
+    bool only_send_active_quest_objectives = false,
+    bool should_update_client_data = false) {
   UpdateOptionsBuilder builder_(_fbb);
+  builder_.add_should_update_client_data(should_update_client_data);
   builder_.add_only_send_active_quest_objectives(only_send_active_quest_objectives);
   builder_.add_only_send_active_quest_description(only_send_active_quest_description);
   return builder_.Finish();
